9ba0ce12e9a6a7e930a13a6cd89fc834d0a9c07c
LLLLLK123 <lyingxuan789@gmail.com>
2025-03-20 08:35:01 +0100

    Tous les tests Cucumber passent avec succès

---
+++ src/main/java/zoo/Pandas.java
@@ -0,0 +1,60 @@
+package zoo;
+
+/**
+ * Write a description of class zoo.Pandas here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Pandas
+{
+    private String name;
+    private int age;
+    private Zoo zoo;
+    private boolean isHungry;
+
+    public Pandas(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.zoo = null;
+        this.isHungry = true;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public Zoo getZoo() { return this.zoo; }
+
+    public boolean isHungry() { return this.isHungry; }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setZoo(Zoo zoo) {
+        this.zoo = zoo;
+    }
+
+    public void eat() {
+        this.isHungry = false;
+        System.out.println(name + " is eating bamboo.");
+    }
+
+    public void sleep() {
+        this.isHungry = true;
+        System.out.println(name + " is sleeping in the trees.");
+    }
+
+    public String makeSound() {
+        return name + " squeaks!";
+    }
+}
